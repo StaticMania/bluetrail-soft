7773ded68a52fa58c8464fe3aeb7500815cddbe0
Arifulislam5577 <arifalamm33@gmail.com>
2025-09-03 22:19:20 +0600

    added: favicon added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import Footer from '../components/shared/footer';
 import Navbar from '../components/shared/navbar';
 import SmoothScrollProvider from '../components/shared/smooth-scroll';
@@ -7,8 +7,9 @@ import { figtree, ibmPlexMono, interTight } from '../utils/font';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Bluetrail Software',
-  description: 'Bluetrail Software',
+  title: 'Blue Trail Software',
+  description:
+    'Blue Trail Software designs &amp; develops custom apps for mobile, web &amp; IoT. We created award-winning products for successful brands and disruptive startups.',
 };
 
 export default function RootLayout({
@@ -19,11 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${figtree.variable} ${ibmPlexMono.variable} ${interTight.variable} bg-background antialiased`}>
-        <SmoothScrollProvider>
-          <Navbar />
-          {children}
-          <Footer />
-        </SmoothScrollProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SmoothScrollProvider>
+            <Navbar />
+            {children}
+            <Footer />
+          </SmoothScrollProvider>
+        </Suspense>
       </body>
     </html>
   );
